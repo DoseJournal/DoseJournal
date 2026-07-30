89bfc4c3071f6d5b707fce2d458595d469c69d95
amyjuliet13-sudo <amyjuliet13@gmail.com>
2026-07-30 20:57:24 +1000

    Split pod install step, fix deployment target

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,15 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.amyjuliet.medledger',
   appName: 'MedLedger',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  },
+  ios: {
+    minVersion: '14.0'
+  }
 };
 
 export default config;
